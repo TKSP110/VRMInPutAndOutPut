e8e633d355a945c5580619122a823fb07fbbe8b8
TKSP <ymtkyorosiku.tut@gmail.com>
2019-07-20 13:23:17 +0900

    SampleGame2D追加

--- Assets/VRMIO/Sample/Scripts/Sample2DGame.cs
+++ Assets/VRMIO/Sample/Scripts/Sample2DGame.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 #if VRMIO
 using UniRx.Async;
 using VRMIO;
+using UnityChan;
 #endif
 public class Sample2DGame : MonoBehaviour
 {
 
+
+    [Header("アニメーションコントローラー")]
+    [SerializeField]
+    private RuntimeAnimatorController VRoidLocomotion;
+
+    [Header("ロード用キャンバス")]
     [SerializeField]
     private Canvas LoadingCanvas;
 
@@ -36,7 +43,7 @@ public class Sample2DGame : MonoBehaviour
         if (Player != null)
             Destroy(Player);
 
-        Player = await VRMLoader.InstantiateVRM(data.vrmdata);
+        Player = await VRMLoader.InstantiateVRM(data.vrmdata,Vector3.zero,Quaternion.Euler(0, 90, 0));
 
         LoadingCanvas.enabled = false;
 
@@ -45,11 +52,60 @@ public class Sample2DGame : MonoBehaviour
         //リジッドボディ
         Rigidbody2D rb = Player.AddComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        //カメラをプレイヤーの子へ
-        Camera.main.transform.SetParent(Player.transform);
 
+
+        //unitychanのサンプルシーンと同じ状況を作る
+
+        //UnityChanControlScriptWithRgidBodyの改変版
+        VRoidControllerSample2D controller = Player.AddComponent<VRoidControllerSample2D>();
+
+        controller.useCurves = false;
+
+        //空のオブジェクト生成
+        GameObject standardPos = new GameObject("CamPos");
+        GameObject frontPos = new GameObject("FrontPos");
+        GameObject jumpPos = new GameObject("JumpPos");
+
+        //子に設定し、座標移動
+        standardPos.transform.SetParent(Player.transform);
+        standardPos.transform.localPosition = new Vector3(5, 1.25f, 0);
+        standardPos.transform.localRotation = Quaternion.Euler(7.5f, 270f, 0);
+        frontPos.transform.SetParent(Player.transform);
+        frontPos.transform.localPosition = new Vector3(-2.651793f, 1.367743f, 0.1f);
+        frontPos.transform.localRotation = Quaternion.Euler(6.271f, 180, 0);
+        jumpPos.transform.SetParent(Player.transform);
+        //ジャンプ時のカメラが向かう座標
+        jumpPos.transform.localPosition = new Vector3(0, 5, 0);
+        jumpPos.transform.localRotation = Quaternion.Euler(-5 , 90, 0);
+
+        //アニメーションコントローラー設定
+        Player.GetComponent<Animator>().runtimeAnimatorController = VRoidLocomotion;
+
+        //カメラを設定
+        Camera.main.gameObject.AddComponent<ThirdPersonCamera>();
+        standardPosT = standardPos.transform;
+        frontPosT = frontPos.transform;
+        startrStandardPos = standardPosT.position;
+        startFrontPos = frontPosT.position;
+        startrStandardRota = standardPosT.rotation;
+        startFrontRota = frontPosT.rotation;
     }
 #endif
+
+    private Transform standardPosT;
+    private Transform frontPosT;
+    private Vector3 startrStandardPos;
+    private Vector3 startFrontPos;
+    private Quaternion startrStandardRota;
+    private Quaternion startFrontRota;
+    private void FixedUpdate()
+    {
+        standardPosT.position = new Vector3(standardPosT.position.x, standardPosT.position.y, startrStandardPos.z);
+        frontPosT.position = new Vector3(frontPosT.position.x, frontPosT.position.y, startFrontPos.z);
+        standardPosT.rotation = startrStandardRota;
+        frontPosT.rotation = startFrontRota;
+    }
+
     public void OnBackTitleButton()
     {
         SceneManager.LoadScene("SampleTitle");
